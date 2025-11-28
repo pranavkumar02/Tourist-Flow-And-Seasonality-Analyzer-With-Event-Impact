177d0be0b535b529fe878e5c1d2f264a44c101bc
sunkara harika <Harikasunkara2003@gmail.com>
2025-11-29 00:30:29 +0530

    Add files via upload

--- app/src/db.py
+++ app/src/db.py
@@ -1,1 +1,18 @@
+# db.py
+import os
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
 
+# load .env file
+load_dotenv()
+
+def get_engine():
+    db_user = os.getenv("DB_USER")
+    db_password = os.getenv("DB_PASSWORD")
+    db_host = os.getenv("DB_HOST")      # RDS endpoint
+    db_port = os.getenv("DB_PORT", "5432")
+    db_name = os.getenv("DB_NAME")      # your DB name (tourism_project)
+
+    db_url = f"postgresql+psycopg2://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
+    engine = create_engine(db_url)
+    return engine
